a43506ca344e65bb9d587ad3f27e7013de025269
nimrab <nimrab.0@gmail.com>
2022-03-03 23:09:27 +0300

    routes login fixed

--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -49,7 +49,7 @@ export const Login = () => {
     })
 
     if (isLoggedIn) {
-        return <Navigate to='/todolist47'/>
+        return <Navigate to='/'/>
     }
 
     return <Grid container justifyContent={'center'}>
